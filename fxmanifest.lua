e05b9857bc204328c772c3503786c0ae56737376
Freech <144473764+freech-dev@users.noreply.github.com>
2024-03-22 15:56:35 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,18 +4,15 @@ games({ 'gta5' })
 
 shared_script({
     'server_config.lua',
-    'client_config.lua',
     'shared_functions.lua'
 });
 
 server_scripts({
     'server.lua',
-    './**/server/**.lua'
 });
 
 client_scripts({
     'client.lua',
-    './**/client/**.lua'
 });
 
 exoports ({
